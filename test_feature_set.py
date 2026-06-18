fefa73d58ecce027220fa9dc61d93cac5796ba95
Yatharth Raghuvanshi <yatharth69raghu@Gmail.com>
2026-06-18 18:40:56 +0530

    Yatharth

--- test_feature_set.py
+++ test_feature_set.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from src.feature_sets import base_features
 
-df = pd.read_csv("data/rolling_ai4i.csv")
+df = pd.read_csv("C:/Users/ASUS/Downloads/project-1-iot/data/cleaned_ai4i.csv")
 
 missing = [col for col in base_features if col not in df.columns]
 
