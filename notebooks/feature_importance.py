cae5c69882d46f60a193682e5087ac051d1d6203
Yatharth Raghuvanshi <yatharth69raghu@Gmail.com>
2026-06-19 21:14:55 +0530

    Yatharth

---
+++ notebooks/feature_importance.py
@@ -0,0 +1,59 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+
+df = pd.read_csv("data/context_updated.csv")
+
+features = [
+    "Type_enc",
+    "Air temperature [K]",
+    "Process temperature [K]",
+    "Rotational speed [rpm]",
+    "Torque [Nm]",
+    "Tool wear [min]",
+    "ambient_temp_C",
+    "factory_load_pct"
+]
+
+X = df[features]
+y = df["Machine failure"]
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y,
+    test_size=0.2,
+    random_state=42,
+    stratify=y
+)
+
+model = RandomForestClassifier(
+    n_estimators=100,
+    random_state=42
+)
+
+model.fit(X_train, y_train)
+
+importance = pd.DataFrame({
+    "Feature": features,
+    "Importance": model.feature_importances_
+})
+
+importance = importance.sort_values(
+    by="Importance",
+    ascending=False
+)
+
+print(importance)
+
+plt.figure(figsize=(10,5))
+plt.bar(
+    importance["Feature"],
+    importance["Importance"]
+)
+plt.xticks(rotation=45)
+plt.tight_layout()
+plt.savefig(
+    "outputs/feature_importance.png"
+)
+plt.show()
