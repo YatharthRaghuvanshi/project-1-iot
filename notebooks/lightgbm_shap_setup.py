9bf1770b53bc3c036b1266e931bb9b92e2b28cbb
Yatharth Raghuvanshi <yatharth69raghu@Gmail.com>
2026-06-26 00:46:37 +0530

    Yatharth

--- notebooks/lightgbm_shap_setup.py
+++ notebooks/lightgbm_shap_setup.py
@@ -1,10 +1,10 @@
 import pandas as pd
 from lightgbm import LGBMClassifier
 
-# Load dataset
+
 df = pd.read_csv("data/context_updated.csv")
 
-# Clean column names for LightGBM
+
 df.columns = (
     df.columns
       .str.replace("[", "", regex=False)
@@ -12,13 +12,11 @@ df.columns = (
       .str.replace(" ", "_", regex=False)
 )
 
-# Remove non-numeric columns
+
 numeric_df = df.select_dtypes(include=["int64", "float64"])
 
-# Target column
 y = numeric_df["Machine_failure"]
 
-# Features
 X = numeric_df.drop(columns=["Machine_failure"])
 
 print("Features being used:")
@@ -27,7 +25,6 @@ print(X.columns.tolist())
 print("\nDataset Shape:")
 print(X.shape)
 
-# Train LightGBM
 model = LGBMClassifier(
     random_state=42,
     verbose=-1
