590154c67cf5b50d4aeec2741e8127be64672b0f
Yatharth Raghuvanshi <yatharth69raghu@Gmail.com>
2026-06-28 19:53:51 +0530

    Yatharth

---
+++ notebooks/shap_summary.py
@@ -0,0 +1,48 @@
+import pandas as pd
+import shap
+import matplotlib.pyplot as plt
+
+from lightgbm import LGBMClassifier
+
+df = pd.read_csv("data/context_updated.csv")
+
+df.columns = (
+    df.columns
+      .str.replace("[", "", regex=False)
+      .str.replace("]", "", regex=False)
+      .str.replace(" ", "_", regex=False)
+)
+
+numeric_df = df.select_dtypes(
+    include=["int64", "float64"]
+)
+
+X = numeric_df.drop(
+    columns=["Machine_failure"]
+)
+
+y = numeric_df["Machine_failure"]
+
+model = LGBMClassifier(
+    random_state=42,
+    verbose=-1
+)
+
+model.fit(X, y)
+
+explainer = shap.TreeExplainer(model)
+
+shap_values = explainer.shap_values(X)
+
+shap.summary_plot(
+    shap_values,
+    X,
+    show=False
+)
+
+plt.savefig(
+    "outputs/shap_summary.png",
+    bbox_inches="tight"
+)
+
+print("Saved shap_summary.png")
